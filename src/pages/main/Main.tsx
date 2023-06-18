55d7e958689f5abbcbe87db78308fb50873a522d
MinTo96 <xpeed96@gmail.com>
2023-06-18 18:41:50 +0900

    Feat: 검색창 및 카테고리 HTML 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -27,6 +27,21 @@ export default function Main(): JSX.Element {
       <Header>토픽 사이트</Header>
       <section>
         <article>
+          <div>
+            <form action="get">
+              <input type="text" placeholder="토픽 제목 검색" />
+            </form>
+          </div>
+          <select name="category">
+            <option value="전체">전체</option>
+            <option value="입문">입문</option>
+            <option value="초급">초급</option>
+            <option value="중급">중급</option>
+            <option value="중고급">중고급</option>
+            <option value="고급">고급</option>
+          </select>
+        </article>
+        <article>
           <ItemContainer>
             {topics
               ? topics.map((topic: TopicType) => (
