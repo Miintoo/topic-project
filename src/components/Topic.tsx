8395b6c8d3b56adfe1faca607ed66947d30b4546
MinTo96 <xpeed96@gmail.com>
2023-06-18 14:19:07 +0900

    Feat: Topic 컴포넌트 type 추가, props 작성, HTML 작성

--- src/components/Topic.tsx
+++ src/components/Topic.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Topic(): JSX.Element {
-  return <div>토픽입니다.</div>;
+type TopicType = {
+  title: string;
+  imgPath: string;
+  grade: string;
+};
+
+export default function Topic({ title, imgPath, grade }: TopicType): JSX.Element {
+  return (
+    <li>
+      <p>
+        <img src={imgPath} alt="토픽 이미지" />
+      </p>
+      <p>{title}</p>
+      <p>{grade}</p>
+    </li>
+  );
 }
