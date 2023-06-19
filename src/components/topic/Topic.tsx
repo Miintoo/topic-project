40146a784eb639da49bc5a35a66d0f8b730ac91f
Minto <xpeed96@gmail.com>
2023-06-19 22:22:46 +0900

    Feat: heart icons 추가 및 스타일링 및  onClick 이벤트 로직 추가

--- src/components/topic/Topic.tsx
+++ src/components/topic/Topic.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Item, Image, Title, Grade } from './Topic.styled';
+import React, { useState } from 'react';
+import { Item, Image, Title, Grade, StatusContainer, HeartIcons, HeartInIcons } from './Topic.styled';
 
 type TopicType = {
   title: string;
@@ -8,13 +8,22 @@ type TopicType = {
 };
 
 export default function Topic({ title, imgPath, grade }: TopicType): JSX.Element {
+  const [isHeart, setIsHeart] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsHeart((prev) => !prev);
+  };
+
   return (
     <Item>
       <p>
         <Image src={imgPath} alt="토픽 이미지" />
       </p>
       <Title>{title}</Title>
-      <Grade>{grade}</Grade>
+      <StatusContainer>
+        <Grade>{grade}</Grade>
+        {isHeart ? <HeartInIcons onClick={handleClick} /> : <HeartIcons onClick={handleClick} />}
+      </StatusContainer>
     </Item>
   );
 }
