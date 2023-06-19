f0491d0c38c6319bc2583a7ac02472b1184a381a
Minto <xpeed96@gmail.com>
2023-06-20 08:15:49 +0900

    Feat: localstorage를 통한 좋아요 버튼 유지 로직 추가

--- src/components/topic/Topic.tsx
+++ src/components/topic/Topic.tsx
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Item, Image, Title, Grade, StatusContainer, HeartIcons, HeartInIcons } from './Topic.styled';
 
 type TopicType = {
   title: string;
   imgPath: string;
   grade: string;
+  index: string;
 };
 
-export default function Topic({ title, imgPath, grade }: TopicType): JSX.Element {
+export default function Topic({ title, imgPath, grade, index }: TopicType): JSX.Element {
   const [isHeart, setIsHeart] = useState<boolean>(false);
 
+  useEffect(() => {
+    let thumbsState: string | null = localStorage.getItem('thumbsUp');
+    if (!thumbsState) {
+      thumbsState = '';
+      return;
+    }
+
+    if (JSON.parse(thumbsState).includes(index)) {
+      setIsHeart(true);
+    } else {
+      setIsHeart(false);
+    }
+  }, []);
+
   const handleClick = () => {
+    let thumbsUpList: string | null = localStorage.getItem('thumbsUp');
+    if (!thumbsUpList) {
+      thumbsUpList = '[]';
+    }
+    const parseThumbsUpList: string[] = JSON.parse(thumbsUpList);
+    if (isHeart) {
+      const filterList = parseThumbsUpList?.filter((item: string) => item !== index);
+      localStorage.setItem('thumbsUp', JSON.stringify(filterList));
+    } else {
+      parseThumbsUpList.push(index);
+      localStorage.setItem('thumbsUp', JSON.stringify(parseThumbsUpList));
+    }
     setIsHeart((prev) => !prev);
   };
 
